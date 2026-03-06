d554d39315c4b0c9d6af049704923d35ee85701b
MariaAlice12 <mariaelicemelo@gmail.com>
2026-03-06 15:50:01 -0300

    Home com cadastro e login

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,57 @@
-import { useState } from 'react'
-import './style.css'
+import { useState } from "react";
+import "./style.css";
 
 function Home() {
+
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="page">
 
-        <form className="card-form">
+      <div className="auth-card">
 
-          <h1>Cadastro de Atletas</h1>
+        {!isLogin ? (
+          <>
+            <h1>Cadastre-se</h1>
 
-          <input type="text" placeholder="Nome" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="Email" />
+            <input type="tel" placeholder="Telefone" />
+            <input type="password" placeholder="Senha" />
 
-          <button>Cadastrar</button>
+            <select>
+              <option>Administrador</option>
+              <option>Professor</option>
+              <option>Atleta</option>
+            </select>
 
-        </form>
+            <button>Cadastrar</button>
+
+            <p className="switch">
+              Já tem conta?
+              <span onClick={() => setIsLogin(true)}> Entrar</span>
+            </p>
+          </>
+        ) : (
+          <>
+            <h1>Bem-vindo de volta</h1>
+
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+
+            <button>Entrar</button>
+
+            <p className="switch">
+              Não tem conta?
+              <span onClick={() => setIsLogin(false)}> Cadastre-se</span>
+            </p>
+          </>
+        )}
 
       </div>
 
-  )
+    </div>
+  );
 }
 
 export default Home;
